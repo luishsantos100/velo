04129bdfd83125ec3c53d0269e016e4889d9242c
Luis Santos <luishsantos100@gmail.com>
2026-09-21 17:56:22 -0400

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,25 +1,22 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
+/// Arrange
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-  //Checkpoint
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-1F94KU');
-
   await page.getByTestId('search-order-button').click();
-  
+
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-1F94KU');
 
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-
   
 });
